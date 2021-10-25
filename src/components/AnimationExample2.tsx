bd46d822e2fb8b5343e63cb87719321b0df447a6
Temitope Emmanuel OJo <temitopeojo0@gmail.com>
2021-10-25 07:30:45 +0100

    feat: :sparkles: completed feature

--- src/components/AnimationExample2.tsx
+++ src/components/AnimationExample2.tsx
@@ -1,10 +1,15 @@
 import React from "react"
-import Animated, {useSharedValue,useAnimatedGestureHandler, useAnimatedStyle} from "react-native-reanimated"
+import Animated, {
+    useSharedValue, useAnimatedGestureHandler,
+    useAnimatedStyle, withTiming
+} from "react-native-reanimated"
 import { View, StatusBar, SafeAreaView, StyleSheet, useWindowDimensions } from "react-native"
-import { samples } from "../assets/data/waveform.json"
-import { PanGestureHandler } from "react-native-gesture-handler";
+import { samples as oldSamples } from "../assets/data/waveform.json"
+import { PanGestureHandler, TapGestureHandler } from "react-native-gesture-handler";
 import MaskedView from "@react-native-community/masked-view"
 
+
+const samples = oldSamples.slice(0, 500)
 const STICK_WIDTH = 4;
 const STICK_MARGIN = 1;
 const STICK_FULL_WIDTH = STICK_WIDTH + STICK_MARGIN;
@@ -30,93 +35,142 @@ const WaveForm: React.FC<{
     );
 }
 
+function findNearestMultiple(n, multiple) {
+    'worklet';
+    return Math.floor(n / multiple) * multiple;
+}
+
 
 const AnimationExample2 = () => {
-    const {width,...dimensions} = useWindowDimensions();
-
-
+    const { width } = useWindowDimensions();
+    const playing = useSharedValue(true)
+    const sliding = useSharedValue(false)
     const panX = useSharedValue(0)
     const maxPanX = -width;
 
+    const updateProgress = () => {
+        'worklet';
+
+        if (playing.value && !sliding.value && panX.value > maxPanX) {
+            panX.value = withTiming(panX.value - STICK_FULL_WIDTH)
+        }
+    }
+
+    React.useEffect(() => {
+        const interval = setInterval(() => updateProgress(), 150);
+        return () => clearInterval(interval)
+    }, [])
+
     const panGestureHandler = useAnimatedGestureHandler({
-        onStart:(_,ctx:{
-            startX:number
-        }) => {
-            ctx.startX = panX.value
+        onStart(_, context: {
+            startX: number
+        }) {
+            context.startX = panX.value;
+            sliding.value = true;
         },
-        onActive:(evt,ctx) => {
-            const nextPanX = ctx.startX + evt.translationX
-            console.log("this is ctx",nextPanX)
-            
-            if(nextPanX >= 0){
-                panX.value = 0
-            }else if(nextPanX < maxPanX){
-                panX.value = maxPanX
-            }else{
-                panX.value = nextPanX
+        onActive(event, context) {
+            const nextPanX = context.startX + event.translationX;
+
+            if (nextPanX > 0) {
+                panX.value = 0;
+            } else if (nextPanX < maxPanX) {
+                panX.value = maxPanX;
+            } else {
+                panX.value = nextPanX;
             }
+        },
+        onEnd() {
+            panX.value = withTiming(
+                findNearestMultiple(panX.value, STICK_FULL_WIDTH)
+            );
+
+            sliding.value = false;
+        },
+    });
+
+    const tapGestureHandler = useAnimatedGestureHandler({
+        onActive() {
+            playing.value = !playing.value;
         }
     })
 
     const maskAnimatedStyle = useAnimatedStyle(() => {
-        return{
-            transform:[
+        return {
+            transform: [
                 {
                     // Moves the entire waveform from right -> left
-                    translateX:panX.value
+                    translateX: panX.value
                 }
             ]
         }
     })
 
     const playedAnimatedStyle = useAnimatedStyle(() => {
-        return{
-            width:-panX.value
+        return {
+            width: -panX.value
         }
     })
 
+    const topWavesAnimatedStyles = useAnimatedStyle(() => {
+        return {
+            height: withTiming(playing.value ? 50 : 1)
+        }
+    })
 
-
+    const bottomWavesAnimatedStyles = useAnimatedStyle(() => {
+        return {
+            height: withTiming(playing.value ? 40 : 1)
+        }
+    })
 
     return (
         <>
             <StatusBar barStyle="light-content" />
-                <SafeAreaView style={{flex:1,backgroundColor:"black"}}>
-                <PanGestureHandler onGestureEvent={panGestureHandler}>
-                    <Animated.View style={[{flex:1},maskAnimatedStyle]} >
-                        <MaskedView 
-                            maskElement={
-                                <View 
+            <SafeAreaView style={{ flex: 1, backgroundColor: "black" }}>
+                <TapGestureHandler numberOfTaps={1}
+                    onGestureEvent={tapGestureHandler}
+                >
+                    <Animated.View style={{ flex: 1 }}>
+                        <PanGestureHandler onGestureEvent={panGestureHandler}>
+                            <Animated.View style={[{ flex: 1 }, maskAnimatedStyle]} >
+                                <MaskedView
+                                    maskElement={
+                                        <View
+                                            style={{
+                                                backgroundColor: "transparent",
+                                                flex: 1,
+                                                justifyContent: "center"
+                                            }}
+                                        >
+                                            <Animated.View style={topWavesAnimatedStyles}>
+                                                <WaveForm {...{ samples }} />
+                                            </Animated.View>
+                                            <Animated.View style={bottomWavesAnimatedStyles}>
+                                                <WaveForm {...{ samples }} reversed />
+                                            </Animated.View>
+                                        </View>
+                                    }
                                     style={{
-                                        backgroundColor:"transparent",
-                                        flex:1,
-                                        justifyContent:"center"
+                                        height: "100%",
+                                        width: "100%",
+                                        marginLeft: "50%"
                                     }}
                                 >
-                                    <WaveForm {...{ samples }} />
-                                    <WaveForm {...{ samples }} reversed />
-                                </View>
-                            }
-                            style={{
-                                height:"100%",
-                                width:"100%",
-                                marginLeft:"50%"
-                            }}
-                        >
-                            <Animated.View 
-                                style={{
-                                    ...StyleSheet.absoluteFillObject,
-                                    zIndex:1,
-                                    backgroundColor:"#FF5836",
-                                    width:"10%"
-                                }}
-                            />
-                            <View 
-                                style={{flex:1,backgroundColor:"white"}}
-                            />
-                        </MaskedView>
+                                    <Animated.View
+                                        style={[{
+                                            ...StyleSheet.absoluteFillObject,
+                                            zIndex: 1,
+                                            backgroundColor: "#FF5836"
+                                        }, playedAnimatedStyle]}
+                                    />
+                                    <View
+                                        style={{ flex: 1, backgroundColor: "white" }}
+                                    />
+                                </MaskedView>
+                            </Animated.View>
+                        </PanGestureHandler>
                     </Animated.View>
-                </PanGestureHandler>
+                </TapGestureHandler>
             </SafeAreaView>
         </>
     )
@@ -126,12 +180,11 @@ const waveFormStyles = StyleSheet.create({
     container: {
         flexDirection: 'row',
         alignItems: 'flex-end',
-        height: 50,
+        height: "100%",
     },
     containerReversed: {
         alignItems: 'flex-start',
-        opacity: 0.3,
-        height: 40,
+        opacity: 0.3
     },
     stick: {
         backgroundColor: 'white',
